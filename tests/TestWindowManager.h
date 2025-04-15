d579c300f08ea6973e682e4176f0c3841e11d010
Aarya Bhatia <aaryab@amazon.com>
2025-04-15 10:14:54 -0700

    fix compiler errors in test

--- tests/TestWindowManager.h
+++ tests/TestWindowManager.h
@@ -8,37 +8,40 @@ struct TestWindowManager : public IWindowManager
 
     TestWindowManager(Dimension bounds) : bounds(bounds) {}
 
-    Dimension get_bounds() const { return bounds; }
+    Dimension get_bounds() const override { return bounds; }
 
-    void draw() {
+    void draw()override {
         draw_calls++;
     }
 
-    void show() {}
+    void show()override {}
 
-    bool resize(Dimension bounds) {
+    bool resize(Dimension bounds)override {
         this->bounds = bounds;
         return true;
     }
 
-    ContentWindow* get_content_node() {
+    ContentWindow* get_content_node()override {
         return nullptr;
     }
 
-    void set_content(ContentWindow* content_window) {}
+    void set_content(ContentWindow* content_window)override {}
 
-    bool split_vertical(ContentWindow* new_content) {
+    bool split_vertical(ContentWindow* new_content)override {
         return false;
     }
 
-    bool split_horizontal(ContentWindow* new_content) {
+    bool split_horizontal(ContentWindow* new_content)override {
         return false;
     }
 
-    void focus(ContentWindow* node) {}
-    void unfocus(ContentWindow* node) {}
-    ContentWindow* get_content_node_right(Window* current) { return nullptr; }
-    ContentWindow* get_content_node_left(Window* current) { return nullptr; }
-    ContentWindow* get_content_node_top(Window* current) { return nullptr; }
-    ContentWindow* get_content_node_bottom(Window* current) { return nullptr; }
+    void focus(ContentWindow* node)override {}
+    void unfocus(ContentWindow* node)override {}
+    ContentWindow* get_content_node_right(Window* current)override { return nullptr; }
+    ContentWindow* get_content_node_left(Window* current)override { return nullptr; }
+    ContentWindow* get_content_node_top(Window* current)override { return nullptr; }
+    ContentWindow* get_content_node_bottom(Window* current) override { return nullptr; }
+
+    int count_nodes() const override { return 0; }
+    int count_content_nodes() const override { return 0; }
 };
