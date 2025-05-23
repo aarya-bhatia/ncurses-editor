14633befd06fb56ba23a465e99de2d3a8a8748ba
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2025-05-23 08:38:15 -0700

    refactor ncurses window class

--- src/NcursesWindow.h
+++ src/NcursesWindow.h
@@ -1,35 +1,20 @@
 #pragma once
 #include <ncurses.h>
-#include <memory>
 #include "Dimension.h"
-#include "Painter.h"
 
-struct WindowDeleter {
-    void operator()(WINDOW* window) const {
-        if (window) {
-            delwin(window);
-        }
-    }
-};
+struct NcursesWindow {
+    WINDOW* window = nullptr;
 
-struct NcursesWindow : public Painter {
-    std::unique_ptr<WINDOW, WindowDeleter> window;
+    NcursesWindow() {}
 
-    NcursesWindow() {
+    NcursesWindow(Dimension d) {
+        window = newwin(d.height, d.width, d.y, d.x);
     }
 
-    NcursesWindow(Dimension dim) {
-        window = std::unique_ptr<WINDOW, WindowDeleter>(
-            newwin(dim.height, dim.width, dim.y, dim.x)
-        );
+    int height() const { return getmaxy(window); }
+    int width() const { return getmaxx(window); }
 
-        wrefresh(window.get());
-    }
-
-    int height() const { return getmaxy(window.get()); }
-    int width() const { return getmaxx(window.get()); }
-
-    WINDOW* get() const noexcept { return window.get(); }
+    WINDOW* get() const noexcept { return window; }
 
     bool is_valid_position(int y, int x) {
         return x >= 0 && y >= 0 && x < width() && y < height();
@@ -38,17 +23,50 @@ struct NcursesWindow : public Painter {
     void show()
     {
         if (window) {
-            wrefresh(window.get());
+            wrefresh(window);
         }
     }
 
-    void draw_line(int y, const std::string& content);
-    void draw_till_eol(int y, int x, const std::string& content);
-    void draw_character(int y, int x, char c);
-    void clear();
-    void clear_line(int y);
-    void clear_till_eol(int y, int x);
-    void move(int y, int x);
+    void draw_line(int y, const std::string& content) {
+        if (is_valid_position(y, 0)) {
+            mvwprintw(window, y, 0, content.substr(0, width()).c_str());
+        }
+    }
+
+    void draw_till_eol(int y, int x, const std::string& content) {
+        if (is_valid_position(y, x)) {
+            mvwprintw(window, y, x, content.substr(0, width() - x).c_str());
+        }
+
+    }
+
+    void draw_character(int y, int x, char c) {
+        if (is_valid_position(y, x)) {
+            mvwaddch(window, y, x, c);
+        }
+
+    }
+
+    void clear() {
+        wclear(window);
+
+    }
+
+    void clear_line(int y) {
+        wmove(window, y, 0);
+        wclrtoeol(window);
+
+    }
+
+    void clear_till_eol(int y, int x)
+    {
+        wmove(window, y, x);
+        wclrtoeol(window);
+    }
+
+    void move(int y, int x) {
+        wmove(window, y, x);
+    }
 
     void move_cursor(int y, int x) {
         move(y, x);
@@ -56,7 +74,7 @@ struct NcursesWindow : public Painter {
 
     void draw_border() {
         if (window) {
-            box(window.get(), '|', '-');
+            box(window, '|', '-');
         }
     }
 };
